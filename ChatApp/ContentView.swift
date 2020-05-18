d6c27b8214a8e9b2b1124957a21a110ec2c0b916
notyourfavouriteprogrammer <45564583+ryanda9910@users.noreply.github.com>
2020-05-18 22:59:29 +0700

    Update ContentView.swift

--- ChatApp/ContentView.swift
+++ ChatApp/ContentView.swift
@@ -1,8 +1,8 @@
 //
-//  ContentView.swift
+//  AppDelegate.swift
 //  ChatApp
 //
-//  Created by NusaFox on 18/05/20.
+//  Created by Aldo Ryanda on 18/05/20.
 //  Copyright © 2020 none. All rights reserved.
 //
 
